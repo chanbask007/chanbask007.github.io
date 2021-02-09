389362a043a45f0286f4ef0b7646f243701c4aa4
Chanbas Kalge <chanbask007@gmail.com>
2021-02-09 13:41:32 +0530

    deploy.sh conf changed

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env sh
 
-#abort on errors
+# abort on errors
 set -e
 
-#build
+# build
 npm run build
 
-#navigate into the build output repository
+# navigate into the build output directory
 cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
 
 git init
 git add -A
-git commit -m "deployed"
+git commit -m 'deploy'
 
+# if you are deploying to https://<USERNAME>.github.io
 git push -f git@github.com:chanbask007/chanbask007.github.io.git master
 
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
 cd -
-
-
